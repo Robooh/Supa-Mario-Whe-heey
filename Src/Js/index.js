4a6396de20744f948a7cf5e627a972251709d917
Davi <122844770+Robooh@users.noreply.github.com>
2023-01-21 21:51:19 -0300

    Add files via upload

---
+++ Src/Js/index.js
@@ -0,0 +1,32 @@
+console.log(document);
+console.log(document.querySelector(".botao-trailer"));
+console.log("botaoTrailer");
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const modal = document.querySelector(".modal");
+const video = document.getElementById("video");
+const linkDoVideo = video.src;
+
+
+
+botaoTrailer.addEventListener("click", () => {
+    modal.classList.add("aberto");
+    video.setAttribute("src", linkDoVideo);
+    
+});
+
+
+
+const botaoFecharModal = document.querySelector(".fechar-modal");
+
+botaoFecharModal.addEventListener("click", () => {
+
+    modal.classList.remove("aberto");
+    video.setAttribute("src", "");
+});
+
+
+
+
+
+
